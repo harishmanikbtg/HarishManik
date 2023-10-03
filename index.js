61dfb35147e0b4b200ad2e846546a98461fe8c47
harishmanikbtg <harishbhurebtg@gmail.com>
2023-10-03 19:34:41 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// // const btnContainer = document.getElementById("nav-list");
+
+// // // Get all buttons with class="btn" inside the container
+// // const btns = btnContainer.getElementsByClassName("nav-link");
+
+// // for()
+
+// const activePage = window.location.pathname;
+// const navLinks = document.querySelectorAll("nav");
+
+// for(let i = 0; i < navLinks.length; i++) {
+//     if(navLinks[i].classList.includes(`${activePage}`)) {
+//         navLinks[i].classList.add("active");
+//     }
+// }
