0a527472c4910231d406d85124a87b657989f78e
harishmanikbtg <harishbhurebtg@gmail.com>
2023-10-09 17:47:13 +0530

    added inventory management system project

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
-let http = require('http');
-let fs = require('fs');
+// let http = require('http');
+// let fs = require('fs');
 
-let server = http.createServer(function(req, res) {
-    let url = req.url
-    if(url.split("/")[1] === "public"){
-        handlePublic(url, req, res);
-    }else{
-        let html = fs.readFileSync("./index.html");
-        res.setHeader("Content-Type", "text/html");
-        res.statusCode = 200;
-        res.end(html);
-    }
+// let server = http.createServer(function(req, res) {
+//     let url = req.url
+//     if(url.split("/")[1] === "public"){
+//         handlePublic(url, req, res);
+//     }else{
+//         let html = fs.readFileSync("./index.html");
+//         res.setHeader("Content-Type", "text/html");
+//         res.statusCode = 200;
+//         res.end(html);
+//     }
 
     
-});
+// });
 
-function handlePublic(url, req, res){
-    let file = fs.readFileSync("./" + url)
-    res.end(file);
-}
+// function handlePublic(url, req, res){
+//     let file = fs.readFileSync("./" + url)
+//     res.end(file);
+// }
 
-server.listen(3000);
+// server.listen(3000);
+
+const sendMail = (mail)=>{
+    fetch('/send', {
+        method: 'post',
+        body: mail,
+    }).then((response) => {
+        return response.json();
+    });
+};
+
+
+const form = document.getElementById("contact-form");
+
+const formEvent = form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let mail = new FormData(form);
+
+    sendMail(mail);
+})
