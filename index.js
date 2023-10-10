464f5c70baa553148c37294e108a53d11b99106a
harishmanikbtg <harishbhurebtg@gmail.com>
2023-10-10 19:17:14 +0530

    Added cotact form functionality using emailjs

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-// let http = require('http');
-// let fs = require('fs');
-
-// let server = http.createServer(function(req, res) {
-//     let url = req.url
-//     if(url.split("/")[1] === "public"){
-//         handlePublic(url, req, res);
-//     }else{
-//         let html = fs.readFileSync("./index.html");
-//         res.setHeader("Content-Type", "text/html");
-//         res.statusCode = 200;
-//         res.end(html);
-//     }
-
-    
-// });
-
-// function handlePublic(url, req, res){
-//     let file = fs.readFileSync("./" + url)
-//     res.end(file);
-// }
-
-// server.listen(3000);
-
-const sendMail = (mail)=>{
-    fetch('/send', {
-        method: 'post',
-        body: mail,
-    }).then((response) => {
-        return response.json();
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_hft0lkd", "template_ydc9w3w", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
     });
 };
-
-
-const form = document.getElementById("contact-form");
-
-const formEvent = form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let mail = new FormData(form);
-
-    sendMail(mail);
-})
