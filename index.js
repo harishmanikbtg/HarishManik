34a93cfe911874d9abdd44e4fd58bd6e148a0b1e
Harish Manik <harishbhurebtg@gmail.com>
2023-11-07 19:55:44 +0530

    replaced the navbar with my custom navigation bar

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onload = function () {
       emailjs.sendForm("service_hft0lkd", "template_ydc9w3w", this).then(
         function () {
           alert("Message sent!");
-          document.getElementById('contactForm').reset();
+          document.getElementById("contactForm").reset();
         },
         function (error) {
           console.log("FAILED...", error);
@@ -19,23 +19,57 @@ window.onload = function () {
 };
 
 //selectors
-const themeToggleBtn = document.querySelector('.theme-toggle');
+const themeToggleBtn = document.querySelector(".theme-toggle");
+const smallMenu = document.querySelectorAll(".smallMenu");
+const toggleBtn = document.querySelector("#toggleBtn");
+const leftBtn = document.querySelector("#leftBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const pageBlur = document.querySelector(".pageBlur");
+const smallMenuItem1 = document.querySelector('.smallMenuItem1');
+const smallMenuItem2 = document.querySelector('.smallMenuItem2');
+const smallMenuItem3 = document.querySelector('.smallMenuItem3');
+const smallMenuItem4 = document.querySelector('.smallMenuItem4');
 
 //state
-const theme = localStorage.getItem('theme');
+const theme = localStorage.getItem("theme");
 
 //on mount
 theme && document.body.classList.add(theme);
 
 //handlers
-handleThemeToggle = () =>{
-  document.body.classList.toggle('dark-mode');
-  if(document.body.classList.contains('dark-mode')){
-    localStorage.setItem('theme', 'dark-mode');
-  }else{
-    localStorage.removeItem('theme');
+handleThemeToggle = () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark-mode");
+  } else {
+    localStorage.removeItem("theme");
   }
-}
+};
+
+handleToggleBtn = () => {
+  smallMenu.forEach((item) => {
+    item.style.display = "flex";
+  });
+
+  pageBlur.style.display = "block";
+};
+
+handleCloseBtn = () => {
+  smallMenu.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  pageBlur.style.display = "none";
+};
 
 //events
-themeToggleBtn.addEventListener('click', handleThemeToggle);
+themeToggleBtn.addEventListener("click", handleThemeToggle);
+
+toggleBtn.addEventListener("click", handleToggleBtn);
+
+closeBtn.addEventListener("click", handleCloseBtn);
+
+smallMenuItem1.addEventListener('click', handleCloseBtn);
+smallMenuItem2.addEventListener('click', handleCloseBtn);
+smallMenuItem3.addEventListener('click', handleCloseBtn);
+smallMenuItem4.addEventListener('click', handleCloseBtn);
